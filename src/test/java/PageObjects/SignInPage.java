76cae0a0f2f5f3c1bd0adea4f25749e20042c2d7
Saulius Kucevicius <saulius.kucevicius@ten10.com>
2023-04-17 10:28:21 +0100

    Added TestSuite and PageObjects package

---
+++ src/test/java/PageObjects/SignInPage.java
@@ -0,0 +1,16 @@
+package PageObjects;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class SignInPage {
+
+}
--- src/test/java/TestSuite/Ten10Store.java
+++ src/test/java/TestSuite/Ten10Store.java
@@ -1,15 +1,17 @@
+package TestSuite;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.junit.Test;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
-public class SignInPage {
+public class Ten10Store {
     @Test
     public void incorrectUser() {
         //1.Set the base URI
